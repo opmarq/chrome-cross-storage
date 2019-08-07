2d7b9c6b33b9aace1f796d405fc5bec957e51afe
omar chajia <omar.chajia@mirakl.com>
2019-08-07 19:45:19 +0200

    :poop: define chrome logic

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,59 @@
-const GOOGLE_CHROME_STORAGE: string = 'GOOGLE_CHROME_STORAGE';
-const LOCAL_STORAGE: string = 'LOCAL_STORAGE';
-
 class Storage {
+  private GOOGLE_CHROME_STORAGE: string = 'GOOGLE_CHROME_STORAGE';
+  private LOCAL_STORAGE: string = 'LOCAL_STORAGE';
   private storageMethod: string;
 
   constructor(type: string) {
-    this.storageMethod = type === 'chrome' ? GOOGLE_CHROME_STORAGE : LOCAL_STORAGE;
+    this.storageMethod = type === 'chrome' ? this.GOOGLE_CHROME_STORAGE : this.LOCAL_STORAGE;
   }
 
-  public setStorageMethod(type: string) {
-    this.storageMethod = type === 'chrome' ? GOOGLE_CHROME_STORAGE : LOCAL_STORAGE;
-  }
-
-  public set(key: string, value: string, callback: (msg: any) => void) {
-    if (this.storageMethod === GOOGLE_CHROME_STORAGE) {
+  public set(key: string, value: string, callback: () => void) {
+    if (this.storageMethod === this.GOOGLE_CHROME_STORAGE) {
+      chrome.storage.sync.set(
+        {
+          [key]: value,
+        },
+        callback,
+      );
     } else {
       localStorage.setItem(key, value);
-      callback(key);
+      callback();
     }
   }
 
-  public get(key: string) {
-    if (this.storageMethod === GOOGLE_CHROME_STORAGE) {
+  public get(keys: string | string[], callback: (arg: any) => void): void {
+    if (this.storageMethod === this.GOOGLE_CHROME_STORAGE) {
+      chrome.storage.sync.get(keys, callback);
     } else {
-      localStorage.getItem(key);
+      if (typeof keys === 'string') {
+        const result = localStorage.getItem(keys);
+        callback(result);
+      } else {
+        const result = keys.map(key => localStorage.getItem(key));
+        callback(result);
+      }
     }
   }
 
-  public remove(key: string) {
-    if (this.storageMethod === GOOGLE_CHROME_STORAGE) {
+  public remove(keys: string | string[], callback: () => void): void {
+    if (this.storageMethod === this.GOOGLE_CHROME_STORAGE) {
+      chrome.storage.sync.remove(keys, callback);
     } else {
-      localStorage.removeItem(key);
+      if (typeof keys === 'string') {
+        localStorage.removeItem(keys);
+      } else {
+        keys.map(key => localStorage.removeItem(key));
+      }
+      callback();
     }
   }
 
-  public clear() {
-    if (this.storageMethod === GOOGLE_CHROME_STORAGE) {
+  public clear(callback: () => void): void {
+    if (this.storageMethod === this.GOOGLE_CHROME_STORAGE) {
+      chrome.storage.sync.clear(callback);
     } else {
       localStorage.clear();
+      callback();
     }
   }
 }
